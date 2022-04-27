c680bbb6c09bf4f5b6c42d9882b1a94cfc9ee63b
Mike <nux7jr@gmail.com>
2022-04-27 18:51:45 +0700

    1.2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,23 @@
 const menu = () => {
-  const dropDownMenuBnt = document.querySelector(".dropdown-btn");
-
-  const toggleDropDowpMenu = function () {
-    const dropDownMenu = document.querySelector(".drowdown-menu");
-    dropDownMenu.classList.toggle("drowdown-menu--active");
+  const showMenu = () => {
+    const burgerMenuBtn = document.querySelector(".header__btn");
+    const toggleMenu = () => {
+      const menu = document.querySelector(".menu");
+      menu.classList.toggle("menu--active");
+    };
+    burgerMenuBtn.addEventListener("click", toggleMenu);
   };
+  showMenu();
 
-  dropDownMenuBnt.addEventListener("click", toggleDropDowpMenu);
+  const showDropdownMenu = () => {
+    const dropDownMenuBnt = document.querySelector(".dropdown-btn");
+    const toggleDropDowpMenu = () => {
+      const dropDownMenu = document.querySelector(".drowdown-menu");
+      dropDownMenu.classList.toggle("drowdown-menu--active");
+    };
+    dropDownMenuBnt.addEventListener("click", toggleDropDowpMenu);
+  };
+  showDropdownMenu();
 };
 menu();
 
