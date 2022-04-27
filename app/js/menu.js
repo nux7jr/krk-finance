888a3870f412d942823851188870ae4b660f4f4b
Mike <nux7jr@gmail.com>
2022-04-27 20:39:42 +0700

    1.3

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,6 +1,6 @@
 const menu = () => {
   const showMenu = () => {
-    const burgerMenuBtn = document.querySelector(".header__btn");
+    const burgerMenuBtn = document.querySelector(".menu-btn");
     const toggleMenu = () => {
       const menu = document.querySelector(".menu");
       menu.classList.toggle("menu--active");
@@ -8,7 +8,18 @@ const menu = () => {
     burgerMenuBtn.addEventListener("click", toggleMenu);
   };
   showMenu();
+  const animation = () => {
+    const menuBtn = document.querySelector(".menu-btn");
 
+    menuBtn.addEventListener("click", function () {
+      menuBtn.classList.toggle("is--active");
+      menuBtn.classList.add("is--clicked");
+      setTimeout(function () {
+        menuBtn.classList.remove("is--clicked");
+      }, 300);
+    });
+  };
+  animation();
   const showDropdownMenu = () => {
     const dropDownMenuBnt = document.querySelector(".dropdown-btn");
     const toggleDropDowpMenu = () => {
