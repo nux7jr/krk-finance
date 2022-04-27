ffb7ce72e835ae3ebdd194eb118dc07b23079e83
Mike <nux7jr@gmail.com>
2022-04-27 23:38:17 +0700

    1.6
    burger anim

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,26 @@
 const menu = () => {
   const showMenu = () => {
     const burgerMenuBtn = document.querySelector(".menu-btn");
+
     const toggleMenu = () => {
       const menu = document.querySelector(".menu");
       menu.classList.toggle("menu--active");
     };
-    burgerMenuBtn.addEventListener("click", toggleMenu);
-  };
-  showMenu();
-  const animation = () => {
-    const menuBtn = document.querySelector(".menu-btn");
 
-    menuBtn.addEventListener("click", function () {
-      menuBtn.classList.toggle("is--active");
-      menuBtn.classList.add("is--clicked");
+    const animation = (burgerMenuBtn) => {
+      burgerMenuBtn.classList.toggle("is--active");
+      burgerMenuBtn.classList.add("is--clicked");
       setTimeout(function () {
-        menuBtn.classList.remove("is--clicked");
+        burgerMenuBtn.classList.remove("is--clicked");
       }, 300);
+    };
+
+    burgerMenuBtn.addEventListener("click", () => {
+      toggleMenu();
+      animation();
     });
   };
-  animation();
+  showMenu();
   const showDropdownMenu = () => {
     const dropDownMenuBnt = document.querySelector(".dropdown-btn");
     const toggleDropDowpMenu = () => {
